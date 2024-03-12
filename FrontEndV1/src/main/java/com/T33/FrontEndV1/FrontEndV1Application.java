5608762e7341a50182d06df57fcd056d8be7714b
Johanna Mejias <jmejias@wisc.edu>
2024-03-12 13:54:45 -0500

    adding frontend boiletplate

---
+++ FrontEndV1/src/main/java/com/T33/FrontEndV1/FrontEndV1Application.java
@@ -0,0 +1,13 @@
+package com.T33.FrontEndV1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontEndV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontEndV1Application.class, args);
+	}
+
+}
