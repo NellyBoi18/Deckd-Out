593159fef0ea4aa1b9fd99180d6ece612f95bd4d
Alan Lin Zheng <alzheng@wisc.edu>
2024-03-16 15:38:24 -0500

    Update Round.java

--- Object code/Round.java
+++ Object code/Round.java
@@ -1,5 +1,4 @@
 import java.util.Random;
-import java.util.ArrayList;
 
 public class Round {
     private int p1Bid, p2Bid, p3Bid, p4Bid; //prediction of the number of traick a player will win
@@ -13,6 +12,8 @@ public class Round {
     private Trick[] tricks;
     private String startPlayer;
     private Deck deck = new Deck();
+    private Player p1, p2, p3, p4;
+
 
     /**
      * Constructor
@@ -46,6 +47,16 @@ public class Round {
         //determine order of cards played
         //Need a method to loop from 4th player to 1st player ie something like p2 -> p3 -> p4 -> p1
         //add cards to trickCards arraylist
+
+        //trickCards.add(p1.pickCard(card))
+        //p1.removePlayedCard(card);
+        //trickCards.add(p2.pickCard(card))
+        //p2.removePlayedCard(card);
+        //trickCards.add(p3.pickCard(card))
+        //p3.removePlayedCard(card);
+        //trickCards.add(p4.pickCard(card))
+        //p4.removePlayedCard(card);
+
         Trick trick = new Trick(trickCards);
         Card winningCard = trick.winnerOfTrick();
         if(winnerCard.getOwner.equals(player1)){
@@ -70,10 +81,16 @@ public class Round {
         Random rand = new Random();
         int firstPlayer = rand.nextInt(4); //who plays first in a round
         deck.shuffle();
+        
         ArrayList<Card> p1Hand = deck.spadesDeal(player1)
+        p1.setHand(p1Hand);
         ArrayList<Card> p2Hand = deck.spadesDeal(player2)
+        p2.setHand(p2Hand);
         ArrayList<Card> p3Hand = deck.spadesDeal(player3)
+        p3.setHand(p3Hand);
         ArrayList<Card> p4Hand = deck.spadesDeal(player4)
+        p4.setHand(p4Hand);
+        
         //first trick of a round 
         if(firstPlayer == 0){
             startPlayer = player1;
@@ -99,7 +116,15 @@ public class Round {
 
         int team1PT = p1PT + p3PT;
         int team2PT = p2PT + p4PT; 
-        
+
+
+    }
+
+    /**
+     * Method that calculates the points a team earns after a round of Spades
+     * 
+     */
+    public int pointCalc(int team1PT, int team2PT, int team1Pred, int team2Pred){
 
     }
 }
