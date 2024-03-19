5106a9cee31b1fd66a4e0b1abe36248cc8909c92
Eddie Engelman <edengelman@wisc.edu>
2024-03-19 16:21:44 -0500

    Added comments to Round.java Added setter methods, adjusted pointCalc method, updated more methods

--- Object code/Round.java
+++ Object code/Round.java
@@ -14,8 +14,8 @@ public class Round {
     private Player startPlayer;
     private Deck deck = new Deck(); //the deck of cards
     private Player p1, p2, p3, p4; ////the 4 different players in the game 
-    private int team1OverTricks;
-    private int team2OverTricks;
+    private int team1OverTricks; //the number of overtricks for team 1
+    private int team2OverTricks; //the number of overtricks for team 2
 
 
     /**
@@ -24,10 +24,10 @@ public class Round {
      * 
      * @param team1Pred integer variable of number of tricks the first team will win
      * @param team2Pred integer variable of number of tricks the second team will win
-     * @param player1 string variable of the name of player 1
-     * @param player2 string variable of the name of player 2
-     * @param player3 string variable of the name of player 3
-     * @param player4 string variable of the name of player 4
+     * @param player1 player variable of the name of player 1
+     * @param player2 player variable of the name of player 2
+     * @param player3 player variable of the name of player 3
+     * @param player4 player variable of the name of player 4
      */
     public Round(int team1Pred, int team2Pred, Player p1, Player p2, Player p3, Player p4){
         this.team1Pred = team1Pred;
@@ -42,6 +42,11 @@ public class Round {
 
     /**
      * Method that contains the logic for a trick of Spades
+     * 
+     * @param p1Hand the hand of cards of the first player
+     * @param p2Hand the hand of cards of the second player
+     * @param p3Hand the hand of cards of the third player
+     * @param p4Hand the hand of cards of the fourth player
      */
     public void trickLogicSpades(ArrayList<Card> p1Hand, ArrayList<Card> p2Hand, ArrayList<Card> p3Hand, ArrayList<Card> p4Hand){
         ArrayList<Card> trickCards = new ArrayList<>();
@@ -76,14 +81,17 @@ public class Round {
             p4Tricks++;
         }
         startPlayer = winningCard.getOwner();
+        //TODO SEND WINNING PLAYER TO FRONTEND
     }
 
     /**
      * Method that performs the logic for a round of Spades
+     * 
+     * @return array of two ints, representing the scores of team1 and team2 respectively
      */
     public int[] playRoundSpades(){
         Random rand = new Random();
-        int firstPlayer = rand.nextInt(4); //who plays first in a round
+        int firstPlayer = rand.nextInt(4); //who plays first in the first round
         deck.shuffle();
         
         ArrayList<Card> p1Hand = deck.spadesDeal(p1);
@@ -125,20 +133,24 @@ public class Round {
         }
         
         //calculate scores after all rounds
-        int team1Pts = pointCalc(p1Tricks, p3Tricks, team1Pred, team1OverTricks);
-        int team2Pts = pointCalc(p2Tricks, p4Tricks, team2Pred, team2OverTricks);
+        int team1Pts = pointCalc(p1Tricks, p3Tricks, team1Pred, team1OverTricks, "team1");
+        int team2Pts = pointCalc(p2Tricks, p4Tricks, team2Pred, team2OverTricks, "team2");
         int[] roundPoints = new int[]{team1Pts, team2Pts};
 
         return roundPoints;
-
-
     }
 
     /**
      * Method that calculates the points that a single team earn after a round of Spades
      * 
+     * @param player1Tricks the amount of tricks that the first member of the team has won
+     * @param player2Tricks the amount of tricks that the second member of the team has won
+     * @param prediction the amount of tricks that the team as a whole predicted (player1 bid + player2 bid) 
+     * @param teamOverTricks the amount of overtricks that the team currently has
+     * @param team string representation of the team (team1 vs team2)
+     * @return the final amount of points that a team has earned in a round of play
      */
-    public int pointCalc(int player1Tricks, int player2Tricks, int prediction, int teamOverTricks){
+    public int pointCalc(int player1Tricks, int player2Tricks, int prediction, int teamOverTricks, String team){
         int finalPoints = 0; 
         int totalTricks = player1Tricks + player2Tricks;
         int diff = totalTricks -= prediction;
@@ -153,11 +165,32 @@ public class Round {
             }
         }
 
-        //check for overtricks, if 10+ minus 100 from total score
+        //check for overtricks, if 10+ minus 100 from total score and reset overtrick counter for respective team
         if(teamOverTricks > 10) {
             finalPoints -= 100;
+            if(team.equals("team1")) { setTeam1OvrTricks(0); }
+            if(team.equals("team2")) { setTeam2OvrTricks(0); } 
         }
-
+        //return final amount of points for round
         return finalPoints;
     }
+
+    /**
+     * Sets the number of overtricks for team 1
+     * 
+     * @param num the number of overtricks that team 1 will be set to
+     */ 
+    public void setTeam1OvrTricks(int num) {
+        this.team1OverTricks = num;
+    }
+
+    /**
+     * Sets the number of overtricks for team 2
+     * 
+     * @param num the number of overtricks that team 2 will be set to
+     */ 
+    public void setTeam2OvrTricks(int num) {
+        this.team2OverTricks = num;
+    }
+
 }
