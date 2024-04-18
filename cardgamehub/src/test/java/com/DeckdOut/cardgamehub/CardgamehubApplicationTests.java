05b77a2cff61b08e52b13983a95276a9cfdb9430
Nelson Lu <nwlu@wisc.edu>
2024-04-18 18:29:07 -0500

    Remove old tests

--- cardgamehub/src/test/java/com/DeckdOut/cardgamehub/CardgamehubApplicationTests.java
+++ cardgamehub/src/test/java/com/DeckdOut/cardgamehub/CardgamehubApplicationTests.java
@@ -1,13 +1,13 @@
-package com.DeckdOut.cardgamehub;
+// package com.DeckdOut.cardgamehub;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class CardgamehubApplicationTests {
+// @SpringBootTest
+// class CardgamehubApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
