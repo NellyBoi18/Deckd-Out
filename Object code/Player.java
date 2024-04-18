0b06bdceb383321d4637efa41aac2c1831a2d8cd
Eddie Engelman <edengelman@wisc.edu>
2024-04-18 18:50:44 -0500

    Update Player.java

--- Object code/Player.java
+++ Object code/Player.java
@@ -90,10 +90,16 @@ public class Player {
     }
 
 
-    //HAS TRUMP METHOD
-    public boolean hasTrump(ArrayList<Card> hand) {
+    /**
+     * Method that checks if a player has at least 1 trump card in their hand
+     * 
+     * @param hand the player's hand of cards
+     * @param trump the string of the suit that is trump in the round
+     * @return true is the user has at least 1 trump card in their hand, false otherwise
+     */  
+    public boolean hasTrump(ArrayList<Card> hand, String trump) {
         for(Card card : hand) {
-            if(card.isSpade()) {
+            if(card.getSuit().equals(trump)) {
                 return true;
             }
         }
@@ -113,11 +119,11 @@ public class Player {
 
 
     //Gets the highest value card in a player's hand that isn't trump
-    public Card getHighNorm(Player player) {
+    public Card getHighNorm(Player player, String trump) {
         int max = 0;
         Card finalCard = new Card("Diamonds", 14);
         for(Card card : player.getHand()) {
-            if(card.getCardValue() >= max && !card.isSpade()) {
+            if(card.getCardValue() >= max && !card.getSuit().equals(trump)) {
                 max = card.getCardValue();
                 finalCard = card;
             }
@@ -126,11 +132,11 @@ public class Player {
     }
 
     //get low norm
-    public Card getLowNorm(Player player) {
+    public Card getLowNorm(Player player, String trump) {
         int min = 0;
         Card finalCard = new Card("Diamonds", 1);
         for(Card card : player.getHand()) {
-            if(card.getCardValue() <= min && !card.isSpade()) {
+            if(card.getCardValue() <= min && !card.getSuit().equals(trump)) {
                 min = card.getCardValue();
                 finalCard = card;
             }
@@ -139,10 +145,10 @@ public class Player {
     }
  
     //number of trump cards in hand (in this case it is spades)
-    public int numTrump(Player player) {
+    public int numTrump(Player player, String trump) {
         int counter = 0;
         for(Card card: player.getHand()) {
-            if(card.isSpade()) {
+            if(card.getSuit().equals(trump)) {
                 counter += 1;
             }
         }
@@ -161,9 +167,9 @@ public class Player {
     }
 
     //does an ace card exist in hand?
-    public boolean containsNormAce(Player player) {
+    public boolean containsNormAce(Player player, String trump) {
         for(Card card1 : player.getHand()) {
-            if(card1.getCardValue() == 14 && !card1.isSpade()) {
+            if(card1.getCardValue() == 14 && !card1.getSuit().equals(trump)) {
                 return true;
             }
         }
@@ -223,10 +229,10 @@ public class Player {
 
 
     //returns the number of cards that a player has of the specified value of any suit that is not spades
-    public int numNonTrumpVals(Player player, int cardVal) {
+    public int numNonTrumpVals(Player player, int cardVal, String trump) {
         int counter = 0;
         for(Card card : player.getHand()) {
-            if(!card.getSuit().equals("Spades") && card.getCardValue() == cardVal) {
+            if(!card.getSuit().equals(trump) && card.getCardValue() == cardVal) {
                 counter ++;
             }
         }
@@ -234,7 +240,88 @@ public class Player {
     }
 
 
+    //returns the number of cards that a player has of the specified value of any suit that is not spades
+    public int numVals(Player player, int cardVal) {
+        int counter = 0;
+        for(Card card : player.getHand()) {
+            if(card.getCardValue() == cardVal) {
+                counter ++;
+            }
+        }
+        return counter;
+    }
 
+    //check for better bower (right bower)
+    public boolean hasRightBower(Player player, String trump) {
+        for(Card card : player.getHand()) {
+            if(card.getSuit().equals(trump) && card.getCardValue() == 11) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //check for worse bower (left bower)
+    public boolean hasLeftBower(Player player, String trump) {
+        //get left bower suit
+        String complimentSuit = getLeftBowerSuit(trump);
+        for(Card card : player.getHand()) {
+            if(card.getSuit().equals(complimentSuit) && card.getCardValue() == 11) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //get left bower suit
+    public String getLeftBowerSuit(String trump) {
+        String complimentSuit = "";
+        if(trump.equals("Spades")) {
+            complimentSuit = "Clubs";
+        }
+        if(trump.equals("Clubs")) {
+            complimentSuit = "Spades";
+        }
+        if(trump.equals("Hearts")) {
+            complimentSuit = "Diamonds";
+        }
+        if(trump.equals("Diamonds")) {
+            complimentSuit = "Hearts";
+        }
+        return complimentSuit;
+    }
+
+    //get the suits that are not trump
+    public String[] getNonTrumpSuits(String trump) {
+        String[] finalArr = new String[3];
+        if(trump.equals("Spades")) {
+            finalArr[0] = "Clubs";
+            finalArr[1] = "Hearts";
+            finalArr[3] = "Diamonds";
+            return finalArr;
+        }
+        else if(trump.equals("Clubs")) {
+            finalArr[0] = "Spades";
+            finalArr[1] = "Hearts";
+            finalArr[3] = "Diamonds";
+            return finalArr;
+        }
+        else if(trump.equals("Hearts")) {
+            finalArr[0] = "Clubs";
+            finalArr[1] = "Spades";
+            finalArr[3] = "Diamonds";
+            return finalArr;
+        }
+        else {
+            finalArr[0] = "Clubs";
+            finalArr[1] = "Hearts";
+            finalArr[3] = "Spades";
+            return finalArr;
+        }     
+    }
+
+
+    
 
 
 
@@ -262,17 +349,17 @@ public class Player {
             }
 
             //case2: play low spades if have 5+
-            if(numTrump(cpu) >= 5) {
+            if(numTrump(cpu, "Spades") >= 5) {
                 return getLowSuit(cpu, "Spades");
             }
 
             //case4: play highest spade
-            if(numTrump(cpu) >= 1) {
+            if(numTrump(cpu, "Spades") >= 1) {
                 return getHighSuit(cpu, "Spades");
             }
 
             //case5: play highest non-trump 
-            return getHighNorm(cpu);
+            return getHighNorm(cpu, "Spades");
         }
 
         //CPU doesn't have lead
@@ -284,12 +371,12 @@ public class Player {
             //CPU doesn't have leading suit 
             else {
                 //case1: play highest trump (can scope up)
-                if(hasTrump(cpuHand)) {
+                if(hasTrump(cpuHand, "Spades")) {
                     return getHighSuit(cpu, "Spades");
                 }
 
                 //case2: play lowest non-trump and non-suit
-                return getLowNorm(cpu);
+                return getLowNorm(cpu, "Spades");
             }
         }
         //return null;
@@ -301,13 +388,11 @@ public class Player {
     public int cpuChooseBid(Player cpu) {
         int finalBid = 1;
         ArrayList<Card> cpuHand = cpu.getHand();
-
         //guarantueed tricks are non-trump aces, royal trumps
         //probably tricks are non-trump royals
-
         
         //add any non-trump aces the user has as predicted tricks
-        finalBid += numNonTrumpVals(cpu, 14);
+        finalBid += numNonTrumpVals(cpu, 14, "Spades");
 
         //add any royal trump
         for(Card card : cpuHand) {
@@ -329,31 +414,94 @@ public class Player {
             }
         }
 
-
         //add 1 trick for any number of non-trump jack-king cards
-        Card highestNorm = getHighNorm(cpu);
+        Card highestNorm = getHighNorm(cpu, "Spades");
         if(highestNorm.getCardValue() > 10 && highestNorm.getCardValue() != 14) {
             finalBid++;
         }
-
-
         return finalBid;
     }
 
 
 
     //returns an array of 2 strings
-    //indes 0: "yes" or "no" based on if they want to play the flippedTrump
+    //indes 0: "Yes" or "No" based on if they want to play the flippedTrump
     //index 1: string of suit that player wants to pick, "Pass" if no good suit to pick
     public String[] cpuSuitDecision(Player cpu, String flippedTrump) {
+        String[] returnVals = new String[2];
+        returnVals[0] = ""; //to avoid null exceptions
+        returnVals[1] = ""; //to avoid null exceptions
         ArrayList<Card> cpuHand = cpu.getHand();
-        String[] finalArr = new String[2];
+
+        //want to play flipped trump
+        if(playDecision(cpu, flippedTrump)) {
+            returnVals[0] = "Yes";
+            returnVals[1] = "Pass";
+            return returnVals;
+        }
+        else { //don't want to play flipped trump
+            //get nontrump suits
+            String[] suits = getNonTrumpSuits(flippedTrump);
+            //decide if want to play non-flipped trump
+            for(String suit : suits) {
+                if(playDecision(cpu, suit)) {
+                    returnVals[0] = "No";
+                    returnVals[1] = suit;
+                    return returnVals; 
+                }
+            }
+            //don't want to play flipped trump and don't want to name trump
+            returnVals[0] = "No";
+            returnVals[1] = "Pass";
+            return returnVals;
+        }
+    }
 
 
-
-
-
-        return finalArr;
+    //choose whether or not to play
+    public boolean playDecision(Player cpu, String trump) {
+        ArrayList <Card> cpuHand = cpu.getHand();
+        
+        //when to play:
+        //both bowers
+        if(hasLeftBower(cpu, trump) && hasRightBower(cpu, trump)) {
+            return true;
+        }
+        //4+ trump 
+        else if(numTrump(cpu, trump) >= 4) {
+            return true;
+        }
+        //3+ aces 
+        else if(numVals(cpu, 14) >= 3) {
+            return true;
+        }
+        //right bauer + 2+ trump
+        else if(hasRightBower(cpu, trump) && numTrump(cpu, trump) >= 2) {
+            return true;
+        }
+        //right bauer + 1+ trump + 1+ aces
+        else if(hasRightBower(cpu, trump) && numTrump(cpu, trump) >=1 && numVals(cpu, 14) >= 1) {
+            return true;
+        }
+        //2+ aces + trump 3+ 
+        else if(numVals(cpu, 14) >= 2 && numTrump(cpu, trump) >= 3) {
+            return true;
+        }
+        //left bauer + 2+ trump
+        else if(hasLeftBower(cpu, trump) && numTrump(cpu, trump) >= 2) {
+            return true;
+        }
+        //left bauer + 1+ ace + king trump
+        else if(hasLeftBower(cpu, trump) && numNonTrumpVals(cpu, 14, trump) >= 1 && hasGivenCard(cpu, trump, 13)) {
+            return true;
+        }
+        //left bauer + 1+ ace + queen trump
+        else if(hasLeftBower(cpu, trump) && numNonTrumpVals(cpu, 14, trump) >= 1 && hasGivenCard(cpu, trump, 12)) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
 
