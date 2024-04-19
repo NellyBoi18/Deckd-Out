a978953fc2c31cee753735851d2331b3911e4f52
Nelson Lu <nwlu@wisc.edu>
2024-04-19 00:20:44 -0500

    Added javadocs

--- cardgamehub/src/main/java/com/DeckdOut/cardgamehub/repository/CardRepository.java
+++ cardgamehub/src/main/java/com/DeckdOut/cardgamehub/repository/CardRepository.java
@@ -2,12 +2,17 @@ package com.DeckdOut.cardgamehub.repository;
 
 import com.DeckdOut.cardgamehub.model.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
 
 /**
- * Repository interface for managing User entities in the database.
+ * Repository interface for managing Card entities in the database.
  */
 public interface CardRepository extends JpaRepository<Card, Integer> {
+
+    /**
+     * Finds a card by its owner.
+     *
+     * @param owner The owner of the card to find.
+     * @return The card with the specified owner, or null if not found.
+     */
     Card findByOwner(String owner);
 }
