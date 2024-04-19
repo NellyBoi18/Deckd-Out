e4d2e36fa8cc5370a3a3d05a58a469d05fa20dfe
Nelson Lu <nwlu@wisc.edu>
2024-04-19 00:25:52 -0500

    Added javadocs

--- cardgamehub/src/main/java/com/DeckdOut/cardgamehub/repository/UserRepository.java
+++ cardgamehub/src/main/java/com/DeckdOut/cardgamehub/repository/UserRepository.java
@@ -2,13 +2,25 @@ package com.DeckdOut.cardgamehub.repository;
 
 import com.DeckdOut.cardgamehub.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
 
 /**
  * Repository interface for managing User entities in the database.
  */
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    /**
+     * Finds a user by username.
+     *
+     * @param username The username of the user to find.
+     * @return The user with the specified username, or null if not found.
+     */
     User findByUsername(String username);
+
+    /**
+     * Finds a user by email.
+     *
+     * @param email The email of the user to find.
+     * @return The user with the specified email, or null if not found.
+     */
     User findByEmail(String email);
 }
