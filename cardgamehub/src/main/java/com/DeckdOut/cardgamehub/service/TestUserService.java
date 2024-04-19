6e3530f492542371c97c0935e98185b124aa83b9
Nelson Lu <nwlu@wisc.edu>
2024-04-19 00:27:19 -0500

    Commented out all TestUser code

--- cardgamehub/src/main/java/com/DeckdOut/cardgamehub/service/TestUserService.java
+++ cardgamehub/src/main/java/com/DeckdOut/cardgamehub/service/TestUserService.java
@@ -1,27 +1,27 @@
-package com.DeckdOut.cardgamehub.service;
+// package com.DeckdOut.cardgamehub.service;
 
-import com.DeckdOut.cardgamehub.model.TestUser;
-import com.DeckdOut.cardgamehub.repository.TestUserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
-import org.springframework.stereotype.Service;
+// import com.DeckdOut.cardgamehub.model.TestUser;
+// import com.DeckdOut.cardgamehub.repository.TestUserRepository;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.data.domain.Sort;
+// import org.springframework.stereotype.Service;
 
-import java.util.List;
+// import java.util.List;
 
-@Service
-public class TestUserService implements TestUserServiceInterface{
+// @Service
+// public class TestUserService implements TestUserServiceInterface{
 
-    @Autowired
-    TestUserRepository testUserRepository;
-    @Override
-    public String addTestUser(TestUser testUser) {
-        TestUser result = testUserRepository.save(testUser);
-        return "Successful";
-    }
+//     @Autowired
+//     TestUserRepository testUserRepository;
+//     @Override
+//     public String addTestUser(TestUser testUser) {
+//         TestUser result = testUserRepository.save(testUser);
+//         return "Successful";
+//     }
 
-    @Override
-    public List<TestUser> findAllTestUser() {
-        return testUserRepository.findAll(Sort.by(Sort.Direction.DESC, "id"));
-    }
-}
+//     @Override
+//     public List<TestUser> findAllTestUser() {
+//         return testUserRepository.findAll(Sort.by(Sort.Direction.DESC, "id"));
+//     }
+// }
 
--- cardgamehub/src/main/java/com/DeckdOut/cardgamehub/service/TestUserServiceInterface.java
+++ cardgamehub/src/main/java/com/DeckdOut/cardgamehub/service/TestUserServiceInterface.java
@@ -1,10 +1,10 @@
-package com.DeckdOut.cardgamehub.service;
+// package com.DeckdOut.cardgamehub.service;
 
-import com.DeckdOut.cardgamehub.model.TestUser;
+// import com.DeckdOut.cardgamehub.model.TestUser;
 
-import java.util.List;
+// import java.util.List;
 
-public interface TestUserServiceInterface {
-    public String addTestUser(TestUser testUser);
-    public List<TestUser> findAllTestUser();
-}
+// public interface TestUserServiceInterface {
+//     public String addTestUser(TestUser testUser);
+//     public List<TestUser> findAllTestUser();
+// }
