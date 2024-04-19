6ace625058825d0489a504bf94173e34dd2c7fed
Nelson Lu <nwlu@wisc.edu>
2024-04-19 00:25:58 -0500

    Added javadocs

--- cardgamehub/src/main/java/com/DeckdOut/cardgamehub/service/UserServiceInterface.java
+++ cardgamehub/src/main/java/com/DeckdOut/cardgamehub/service/UserServiceInterface.java
@@ -4,7 +4,22 @@ import com.DeckdOut.cardgamehub.model.User;
 
 import java.util.List;
 
+/**
+ * Interface defining methods for managing User entities.
+ */
 public interface UserServiceInterface {
+    /**
+     * Adds a new user.
+     *
+     * @param user The user to be added.
+     * @return A string indicating the result of the operation.
+     */
     public String addUser(User user);
+
+    /**
+     * Retrieves all users.
+     *
+     * @return A list containing all users.
+     */
     public List<User> findAllUsers();
 }
