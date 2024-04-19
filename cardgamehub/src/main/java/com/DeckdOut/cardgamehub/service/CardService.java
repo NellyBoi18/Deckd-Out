db4b6aad9a79670796dc0b348680fb649675b7f1
Nelson Lu <nwlu@wisc.edu>
2024-04-18 19:59:54 -0500

    Complete Card Service

--- cardgamehub/src/main/java/com/DeckdOut/cardgamehub/service/CardService.java
+++ cardgamehub/src/main/java/com/DeckdOut/cardgamehub/service/CardService.java
@@ -16,6 +16,11 @@ public class CardService implements CardServiceInterface{
     @Override
     public String addCard(Card card) {
 
+        if(cardRepository.findByOwner(card.getOwner()) != null) return "Card with owner already exists";
+        Card result = cardRepository.save(card);
+        return "Successfull";
+
+
         // if(userRepository.findByUsername(user.getUsername()) != null) return "User with username already exists";
         // if(userRepository.findByEmail(user.getEmail()) != null) return "User with email already exists";
         // User result = userRepository.save(user);
@@ -23,8 +28,18 @@ public class CardService implements CardServiceInterface{
     }
 
     @Override
+    public String removeCard(Card card) {
+
+        if(cardRepository.findByOwner(card.getOwner()) == null) return "Card with owner does not exist";
+        cardRepository.delete(card);
+        return "Successfull";
+
+    }
+
+    @Override
     public List<Card> findAllCards() {
-        // return userRepository.findAll(Sort.by(Sort.Direction.DESC, "id"));
+        
+        return cardRepository.findAll(Sort.by(Sort.Direction.DESC, "id"));
     }
 }
 
